d6c76b769a0c933aa0cfe0e5514bc55e528cfddd
Daniel <danielkaiss@hotmail.com>
2018-07-17 00:21:16 -0300

    Vibração

---
+++ Assets/Scripts/Armadilha.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Armadilha : MonoBehaviour {
+
+	//public static int lives = 2;
+	public long vibra;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			//lives -= 1;
+			Destroy(col.gameObject);
+			//Timecounter.timeLeft = 40.0f;
+			SceneManager.LoadScene("Jogo");
+			Vibration.Vibrate(vibra);
+		}
+	}
+}
